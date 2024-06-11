a284d652b3b56ed54f1a7e4ff02377b2cc46febd
Dubuuu <145317870+chewychae@users.noreply.github.com>
2024-06-11 15:35:27 +0800

    Add files via upload

---
+++ oop_database/OOP_DataBase.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop_database;
+
+/**
+ *
+ * @author Acer
+ */
+public class OOP_DataBase {
+
+    static OPTION Gui;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (Gui == null) {
+            Gui = new OPTION ();
+            Gui.setVisible(true);
+        }
+
+    }
+
+}
